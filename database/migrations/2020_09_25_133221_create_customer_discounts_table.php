e51e36b14ad00f7ee5045ae9ee92e9be856c5564
robert pujante <moralesso152842@gmail.com>
2020-09-25 21:41:17 +0800

    added new models -m

---
+++ database/migrations/2020_09_25_133221_create_customer_discounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_discounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('percentage')->nullable();
+            $table->string('verification')->default(1)->nullable();
+            $table->string('status')->default(1)->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_discounts');
+    }
+}
---
+++ database/migrations/2020_09_25_133853_create_damages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDamagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('damages', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id')->nullable();
+            $table->string('product_name')->nullable();
+            $table->integer('qty')->nullable();
+            $table->string('unit')->nullable();
+            $table->string('description')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('approved_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('damages');
+    }
+}
