932dce5e9c499b12547ba5095bce53640a25f8b3
Robertson Pujante <58919933+RobertPujante@users.noreply.github.com>
2020-09-26 20:34:57 +0800

    new blade template

---
+++ database/migrations/2020_09_26_122451_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->integer('expense_categories_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('cost')->nullable();
+            $table->string('status')->default(1)->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
