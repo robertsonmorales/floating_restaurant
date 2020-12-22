a94839bfcbaece152d6b6b4c466d8cc29c3dc61f
RobertPujante <moralesso152842@gmail.com>
2020-12-23 00:09:18 +0800

    fixed responsive sidebar and dashboard

--- resources/views/includes/vendors.blade.php
+++ resources/views/includes/vendors.blade.php
@@ -1,11 +1,11 @@
-<script src="{{ asset('vendors/sweetalert2/node_modules/sweetalert2/dist/sweetalert2.min.js') }}"></script>
+<!-- <script src="{{ asset('vendors/sweetalert2/node_modules/sweetalert2/dist/sweetalert2.min.js') }}"></script> -->
 
 <script src="{{ asset('vendors/jquery/jquery-3.4.1.min.js') }}"></script>
-<script src="{{ asset('vendors/jquery/jquery-ui.js') }}"></script>
-<link href="{{ asset('vendors/jquery/jquery-ui.css') }}" rel="stylesheet">
+<!-- <script src="{{ asset('vendors/jquery/jquery-ui.js') }}"></script>
+<link href="{{ asset('vendors/jquery/jquery-ui.css') }}" rel="stylesheet"> -->
 
-<script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
+<!-- <script src="{{ asset('vendors/select2/select2.min.js') }}"></script>
 
-<link href="{{ asset('vendors/fontawesome/css/all.css') }}" rel="stylesheet">
+<link href="{{ asset('vendors/fontawesome/css/all.css') }}" rel="stylesheet"> -->
 
 <script src="{{ asset('vendors/agGrid/agGrid.js') }}"></script>
